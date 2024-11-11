78545b592a54a8ecf447d07272c293d09a930f84
LakshanRanasinghe <lakshan20b@gmail.com>
2024-11-11 16:59:39 +0530

    added admin,customer,manager  my account tabs

---
+++ template/woocommerce/myaccount/synergy-group-dashboard/synergy-network-audit-compliance.php
@@ -0,0 +1,146 @@
+<div class="account-text-block light-style">
+  <div class="account-title-block borderb va">
+    <img width="57" src="<?php echo THE_SYNERGY_GROUP_URL; ?>public/img/account/activity_log.svg" class="mr2" alt="activity log icon" />
+    <h5>Audit Log</h5>
+  </div>
+
+  <h6 class="mt25"><strong>Recent Logins</strong></h6>
+  <div class="block-lines media-full">
+    <div class="block-line spb">
+      <div class="line-left va">
+        <div class="line-icon2">
+          <img src="<?php echo THE_SYNERGY_GROUP_URL; ?>public/img/account/arrow_right.svg" alt="arrow right icon" />
+        </div>
+        <p>2024/07/07 - 17:45. IP 123.34.45.56</p>
+      </div>
+    </div>
+
+    <div class="block-line spb">
+      <div class="line-left va">
+        <div class="line-icon2">
+          <img src="<?php echo THE_SYNERGY_GROUP_URL; ?>public/img/account/arrow_right.svg" alt="arrow right icon" />
+        </div>
+        <p>2024/07/07 - 19:42. IP 123.34.45.56</p>
+      </div>
+    </div>
+  </div>
+
+  <h6 class="mt25"><strong>Service Interactions</strong></h6>
+  <div class="block-lines media-full">
+    <div class="block-line spb">
+      <div class="line-left va">
+        <div class="line-icon2">
+          <img src="<?php echo THE_SYNERGY_GROUP_URL; ?>public/img/account/arrow_right.svg" alt="arrow right icon" />
+        </div>
+        <p>22024/07/07 - 17:45. Update 0.2.0231.01</p>
+      </div>
+    </div>
+
+    <div class="block-line spb">
+      <div class="line-left va">
+        <div class="line-icon2">
+          <img src="<?php echo THE_SYNERGY_GROUP_URL; ?>public/img/account/arrow_right.svg" alt="arrow right icon" />
+        </div>
+        <p>2024/07/07 - 19:42. Update 0.2.0231.02</p>
+      </div>
+    </div>
+  </div>
+
+  <h6 class="mt25"><strong>Exchange Transactions</strong></h6>
+  <div class="block-lines media-full">
+    <div class="block-line spb">
+      <div class="line-left va">
+        <div class="line-icon2">
+          <img src="<?php echo THE_SYNERGY_GROUP_URL; ?>public/img/account/arrow_right.svg" alt="arrow right icon" />
+        </div>
+        <p>Transaction 46841521684651</p>
+      </div>
+    </div>
+
+    <div class="block-line spb">
+      <div class="line-left va">
+        <div class="line-icon2">
+          <img src="<?php echo THE_SYNERGY_GROUP_URL; ?>public/img/account/arrow_right.svg" alt="arrow right icon" />
+        </div>
+        <p>Transaction 46841521684652</p>
+      </div>
+    </div>
+
+    <div class="block-line spb">
+      <div class="line-left va">
+        <div class="line-icon2">
+          <img src="<?php echo THE_SYNERGY_GROUP_URL; ?>public/img/account/arrow_right.svg" alt="arrow right icon" />
+        </div>
+        <p>Transaction 46841521684653</p>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+<div class="account-text-block light-style">
+  <div class="account-title-block borderb spb">
+    <div class="title-content va">
+      <img src="<?php echo THE_SYNERGY_GROUP_URL; ?>public/img/account/reports2.svg" alt="reports icon" />
+      <h5>Compliance Reports</h5>
+    </div>
+  </div>
+  
+  <div class="messages">
+
+    <div class="messages-sub-block last-bord">
+      <div class="message-block spb">
+        <div class="message-icon">
+          <img src="<?php echo THE_SYNERGY_GROUP_URL; ?>public/img/account/reports3.svg" alt="report icon "/>
+        </div>
+        <div class="message-text">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ...</p>
+        </div>
+        <div class="btn-block">
+          <a href="#" class="btn">read more</a>
+        </div>
+      </div>
+
+      <div class="message-block spb">
+        <div class="message-icon">
+          <img src="<?php echo THE_SYNERGY_GROUP_URL; ?>public/img/account/reports3.svg" alt="report icon "/>
+        </div>
+        <div class="message-text">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ...</p>
+        </div>
+        <div class="btn-block">
+          <a href="#" class="btn">read more</a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<div class="account-text-block light-style">
+  <div class="account-title-block spb">
+    <div class="title-content va">
+      <img src="<?php echo THE_SYNERGY_GROUP_URL; ?>public/img/account/security.svg" alt="security icon" />
+      <h5>Security Settings</h5>
+    </div>
+  </div>
+  
+  <div class="block-lines mt2">
+
+    <div class="block-line spb small-line">
+      <div class="line-left">
+        <p>Access control and permissions for admin users</p>
+      </div>
+      <div class="line-right va btns-part">
+        <div class="btn-block">
+          <a href="#" class="btn style2">manage</a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+</div>
+
+
+        
